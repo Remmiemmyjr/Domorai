56ed2b359ef67cf20af913a4f39d86db7298b859
Remmiemmyjr <emily@isimar.com>
2021-06-23 02:19:46 -0700

    what was the safety net again

--- DomoraiUnity/Assets/Scripts/SafetyNet.cs
+++ DomoraiUnity/Assets/Scripts/SafetyNet.cs
@@ -7,12 +7,15 @@ public class SafetyNet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //what was this supposed to be
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
